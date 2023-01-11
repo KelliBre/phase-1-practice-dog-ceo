48b6e618480f03d4ec2f351e48e920a10b9d8f7d
KelliBre <Kelli.Brecunier@gmail.com>
2023-01-11 15:41:54 +1000

    commit tree fb65e4f3a93837a620c1cc456caffd907bfa8c14

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 console.log('%c HI', 'color: firebrick')
+
+function getDogImg (){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    fetch(imgUrl)
+    .then(response => response.json())
+    .then(response => {
+        const dogContainer = document.getElementById("dog-image-container");
+        response.message.map(url =>{
+            const img = document.createElement("img");
+            img.src = url;
+            dogContainer.append(img);
+        
+        })
+    });
+    
+}
+
+function getAllDogBreeds(){
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+    fetch(breedUrl)
+    .then(response => response.json())
+    .then(response => {
+        console.log("All Breeds", response.message);
+        const dogList = document.getElementById("dog-breeds");
+        const keys = Object.keys(response.message);
+        console.log(keys);
+        keys.map(breed => {
+            const li = document.createElement("li");
+            li.textContent = breed;
+            dogList.append(li);
+        })
+    })
+
+
+}
+getAllDogBreeds();
+getDogImg();
